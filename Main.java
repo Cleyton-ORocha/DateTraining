fa2940457083e2aa706919bfd3dccb8e9078fc07
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-04-30 10:48:41 -0300

    Transformando ISO8601 em DataType

--- Main.java
+++ Main.java
@@ -10,14 +10,9 @@ public class Main {
 		LocalDateTime d2 = LocalDateTime.now();
 		Instant d3 = Instant.now();
 		
-		LocalDate d4 = LocalDate.parse("2022-02-07");
-		LocalDateTime d5 = LocalDateTime.parse("2021-04-24T02:45:08");
-		
 		System.out.println("d1 = " + d1);
 		System.out.println("d2 = " + d2);
 		System.out.println("d3 = " + d3);
-		System.out.println("d4 = " + d4);
-		System.out.println("d5 = " + d5);
 
 	}
 
