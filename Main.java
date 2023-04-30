3deb5beb3e06bac22b5b61d4c8a1fb49b19486a9
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-04-30 11:00:05 -0300

    Adicionando um globalDate com o GMT 3 horas a frente

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ public class Main {
 		LocalDate d4 = LocalDate.parse("2022-02-07");
 		LocalDateTime d5 = LocalDateTime.parse("2021-04-24T02:45:08");
 		Instant d6 = Instant.parse("2021-04-24T02:45:08Z");
+		Instant d7 = Instant.parse("2021-04-24T02:45:08-03:00");
+
 		
 		System.out.println("d1 = " + d1.toString());
 		System.out.println("d2 = " + d2.toString());
@@ -20,6 +22,7 @@ public class Main {
 		System.out.println("d4 = " + d4.toString());
 		System.out.println("d5 = " + d5.toString());
 		System.out.println("d6 = " + d6.toString());
+		System.out.println("d7 = " + d7.toString());
 
 	}
 
