d36d0114b928f225c0099286769ce78b289c2a6e
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-04-30 12:05:25 -0300

    Traduz os parâmetros para data

--- Main.java
+++ Main.java
@@ -8,7 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DateTimeFormatter  dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
+		DateTimeFormatter  dtf2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
 		//https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 		
 		LocalDate d1 = LocalDate.now();
@@ -21,6 +22,9 @@ public class Main {
 		Instant d7 = Instant.parse("2021-04-24T02:45:08-03:00");
 
 		LocalDate d8 = LocalDate.parse("07/08/2022",dtf1);
+		LocalDateTime d9 = LocalDateTime.parse("07/08/2022 02:30",dtf2);
+		
+		LocalDate d10 = LocalDate.of(2023, 9, 20);
 		
 		System.out.println("d1 = " + d1.toString());
 		System.out.println("d2 = " + d2.toString());
@@ -30,6 +34,10 @@ public class Main {
 		System.out.println("d6 = " + d6.toString());
 		System.out.println("d7 = " + d7.toString());
 		System.out.println("d8 = " + d8.toString());
+		System.out.println("d9 = " + d9.toString());
+		System.out.println("d10 = " + d10.toString());
+
+
 
 
 	}
